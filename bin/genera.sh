70f89c0cae8571160b65a5383169325da7bed4a9
Juan J. Cuéllar <juanjosaurio@gmail.com>
2015-01-19 23:16:59 -0500

    ajuste a ruta web

--- bin/genera.sh
+++ bin/genera.sh
@@ -3,10 +3,11 @@
 APPDIR=$(dirname $(dirname $(which $0)))
 BINDIR=$APPDIR/bin
 TMPDIR=$APPDIR/tmp
+WEBDIR=$APPDIR/web
 OUTPUTDIR=$APPDIR/output
 
 grep "Failed password for " /var/log/secure* | awk '{print $(NF-3)}' | sort -n -u > $TMPDIR/ips.txt
 grep "Failed password for " /var/log/secure* | awk '{print $(NF-5)}' | sort -u > $TMPDIR/users.txt
 
 grep "Failed password for" /var/log/secure* | awk -v cols=$TMPDIR/ips.txt -v rows=$TMPDIR/users.txt -f $BINDIR/genera2.awk > $OUTPUTDIR/file.csv
-cp $OUTPUTDIR/file.csv /var/www/html/test
+cp $OUTPUTDIR/file.csv $WEBDIR
